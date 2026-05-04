e35c75e2c4a03da6cf98231bb29e5d49be43eeca
dritisai <dritisaisivarathri@gmail.com>
2026-05-04 21:52:04 +0530

    chore: clean up tracked files and update .gitignore

--- CheckBcrypt.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class CheckBcrypt {
-  public static void main(String[] args) {
-    System.out.println(new BCryptPasswordEncoder().matches(args[0], args[1]));
-  }
-}
